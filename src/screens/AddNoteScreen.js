b9b41b36b752a9377656d5d83f3504fb750cf60c
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-09-20 13:35:53 +0530

    Added screen to add notes.

---
+++ src/screens/AddNoteScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import AddNoteForm from '../components/AddNoteForm';
+
+export default class AddNoteScreen extends React.Component {
+  render() {
+      return (
+        <AddNoteForm/>
+    );
+  }
+}
