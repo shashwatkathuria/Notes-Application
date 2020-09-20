083803d4e3b21bb62e5d40e4b6a35d1b11c9d9ba
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-09-20 13:35:38 +0530

    Added screen to display all notes.

---
+++ src/screens/NotesScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NoteList from '../components/NoteList';
+
+export default class NotesScreen extends React.Component {
+  render() {
+      return (
+        <NoteList/>
+    );
+  }
+}
