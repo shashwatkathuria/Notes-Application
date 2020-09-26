8592c17fe9b34e9b43f5d49129523a029c3a81ed
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2020-09-26 20:42:24 +0530

    To display horizontal line.

---
+++ src/components/HorizontalLine.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const HorizontalLine = props => (
+  <View style={{ borderBottomColor: 'green', borderBottomWidth: 1 }} />
+);
+
+export default HorizontalLine;
